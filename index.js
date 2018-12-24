2930690aaf36af616bcc01304e454eb49cb0c094
Robert Snyder <r.snyder49@gmail.com>
2018-12-24 20:36:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,32 @@ class BoardMember {
     this.homeState = homeState;
     this.training = training;
   }
+  
+  veto() {
+    return "No, I must disagree";
+  }
+  
+  approve() {
+    return "You can do that!";
+  }
+  
+  doCharity() {
+    return "I like to help people.";
+  }
+  
+  releasePressStatement() {
+    return "You will see great things from Scuber.";
+  }
+  
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+  }
 }
+
+
+
+
+
+
+
+
